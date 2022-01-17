8f37f20f65a4a2ebd617f98c8efcf3a971f9be1f
naveen94-georgian <SellappanSelvaraju.Naveen@mygeorgian.ca>
2022-01-16 21:11:47 -0500

    azappdemo commit four

--- src/main/java/com/naveselv/azureappdemo/AzureappdemoApplication.java
+++ src/main/java/com/naveselv/azureappdemo/AzureappdemoApplication.java
@@ -2,17 +2,13 @@ package com.naveselv.azureappdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
 @SpringBootApplication
 public class AzureappdemoApplication {
-
-	@GetMapping("/")
-	public String redirectReq(){
-		return "main";
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(AzureappdemoApplication.class, args);
 	}
--- src/main/java/com/naveselv/azureappdemo/controller/AzureAppController.java
+++ src/main/java/com/naveselv/azureappdemo/controller/AzureAppController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/main")
+@RequestMapping("/")
 public class AzureAppController {
     @GetMapping("/")
     public ResponseEntity<String> get(){
